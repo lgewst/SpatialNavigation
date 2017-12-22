db1090cc29571fc13d3219ed15fcda85aa7d2da3
93lemondrops <93lemondrops@gmail.com>
2017-12-22 14:42:56 +0900

    Added search feature (by TH)

---
+++ js/myLittleSearch.js
@@ -0,0 +1,19 @@
+$(function() {
+	$(".searchbutton").click(function() {
+		var tag = $(".searchbox").val();
+		var tagurl = "./tag_page.html?tag=" + tag;
+
+		window.location.replace(tagurl);
+	});
+
+	$(".searchbox").keydown(function (key) {
+		if(key.keyCode == 13){
+			if($(".searchbox").is(":focus")) {
+				var tag = $(".searchbox").val();
+				var tagurl = "./tag_page.html?tag=" + tag;
+
+				window.location.replace(tagurl);
+			}
+		}
+	});
+});
